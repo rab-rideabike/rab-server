8d651cc3b18045fbaa2962c747bef58776eaffd1
Gupta Pandiri <ratnamgupta.pandiri@cdk.com>
2022-06-07 08:43:40 +0530

    adding new route

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('This is the Home Page of the Ride A Bike Server');
-})
+});
+
+app.get('/new', (req, res) => {
+    res.send('This is the New Route of the Ride A Bike Server');
+});
+
 
 app.listen(5051, ()=> {
     console.log('Listening on the Port 5051')
